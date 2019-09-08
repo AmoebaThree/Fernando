21d114479366801f0344f5d91c4e6c7e7065ebb8
Chris Alexander <chris@chris-alexander.co.uk>
2019-09-08 16:35:53 +0100

    Initio lib update, optional args

--- fernando.py
+++ fernando.py
@@ -2,7 +2,7 @@ if __name__ == '__main__':
     import systemd.daemon, initio, redis
 
     print('Startup')
-    initio.init();
+    initio.init(Motors=True);
     r = redis.Redis(host='192.168.0.1', port=6379, db=0)
     p = r.pubsub(ignore_subscribe_messages=True)
     p.subscribe('fernando')
